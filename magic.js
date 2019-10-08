13244e77d0f98eda41212eb3582b4f195fc27615
Alex Abushady <aabushady214@gmail.com>
2019-10-08 12:32:39 -0400

    Update magic.js
    Cleaned up code, wrapped in a function, added use strict.

--- magic.js
+++ magic.js
@@ -1,21 +1,17 @@
-/*
-The date June 10, 1960 is special because when we write it in the following 
-format, the month times, the day equals the year.
+/*global console*/
+/*jshint esversion:6*/
 
-6/10/60
 
-Write a program that uses 3 variables, month, day, and year with corresponding 
-values of 10, 17, 75. The program should then determine whether the month times the 
-day is equal to the year. If so, it should display a message saying the date is magic, 
-Otherwise, it should display a message saying the date is not magic.
-*/
+(function () {
+    'use strict';
+    // 6/10/60 is a magic date because month * day === year.
+    var month = 3;
+    var day = 30;
+    var year = 90;
 
-var month = 10;
-var day = 17;
-var year = 75;
-
-if (month * day == year) {
-    console.log("Date is Magic!");
-} else {
-    console.log("Date is Not Magic...");
-}
+    if (month * day === year) {
+        console.log("Date is Magic!");
+    } else {
+        console.log("Date is Not Magic...");
+    }
+})();
